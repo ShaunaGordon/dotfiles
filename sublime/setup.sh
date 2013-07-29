f407b3080bdb5fe16c4f3ee476a24a6a0603c6f2
Shauna Gordon <shauna@shaunagordon.com>
2013-07-29 19:29:39 -0400

    Edited Markdown Sublime text settings. Changed Sublime Test entry to go for ST3

--- sublime/setup.sh
+++ sublime/setup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-# Setup a machine for Sublime Text 2
+# Setup a machine for Sublime Text 3
 set -x
 
 # symlink settings in
-sublime_dir=~/.config/sublime-text-2/Packages
+sublime_dir=~/.config/sublime-text-3/Packages
 current_dir=`pwd`
 mv "$sublime_dir/User" "$sublime_dir/User.backup"
 ln -s "$current_dir/User" "$sublime_dir"
